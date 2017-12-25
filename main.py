a1f6451cca1c1a8f987298858e29068cd29ae220
hunterowens <owens.hunter@gmail.com>
2017-12-25 11:33:33 -0800

    add main

---
+++ main.py
@@ -0,0 +1,34 @@
+import json
+import pythonosc
+import chatbot
+
+def setup():
+    """
+    Create the initial state for the performance"
+    """
+    init_state = "neutral"
+    current_act = 1 
+    ## TODO: Setup Python OSC
+
+def osc_dispatch(msg):
+    """
+    Dispatches a message in state change over OSC to all listeners
+    """
+    ## TODO: Pass Init 
+    ## TODO: Send msg
+    pass
+
+if __name__ == '__main__':
+    setup()
+    if current_act == 1: 
+        pass
+    elif current_act == 2: 
+        ## Act 2
+        chatbot.chat()
+        surface.listen()
+        change_sentiment()
+        osc_dispatch(sentiment)
+        ## TODO: Determine when to change to act_# 
+    elif current_act == 3:
+        ## Act 3
+        osc_dispatch()
