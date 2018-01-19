00f6ea46db095014f00fa45dd22164111e97cfdc
hunterowens <owens.hunter@gmail.com>
2018-01-19 13:30:22 -0700

    adding refactor

---
+++ osc_test.py
@@ -0,0 +1,51 @@
+from pythonosc import dispatcher, osc_server, udp_client, osc_message_builder
+
+from osc_server import  broadcast_state 
+from util import osc_dispatch
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument( "--state", dest="new-state", default='guarded',
+                        help="set teh new state", metavar="STATE")
+    parser.add_argument( "--action", dest="action", default='expectant',
+                        help="set teh new action", metavar="ACTION")
+    parser.add_argument('--surface', action='store_true', default=False, help="send dummy surface data")
+    parser.add_argument('--startsurface', action='store_true', default=False, help="test surface start")
+    parser.add_argument('--resetsurface', action='store_true', default=False, help="test surface reset")
+    parser.add_argument('--stopsurface', action='store_true', default=False, help="test surface stop")
+    # Parse Args
+    args = parser.parse_args()
+    print("Arguments: ", args)
+    args = parser.parse_args()
+    if vars(args)['new-state']:
+        state = vars(args)['new-state']
+        action = vars(args)['action']
+        print('changing state and action to {},{}'.format(state, action))
+        osc_dispatch('/newstate',state)
+        if action == 'talking':
+            osc_dispatch('/talking', 1, ip="0.0.0.0", num_tries=4)
+        elif action == 'thinking':
+            osc_dispatch('/thinking', 1)
+        elif action == 'expectant':
+            osc_dispatch("/silent", 1)
+        elif action == 'start':
+            osc_dispatch("/reset", 1)
+        elif action == 'stop':
+            osc_dispatch("/end", 1)
+        else:
+            print("No Action Recognized")
+    elif args.startsurface:
+        print("Telling surfaces to turn on")
+        osc_dispatch('/startsurface', 1)
+    elif args.closesurface:
+        print("Telling surfaces to close")
+        osc_dispatch('/closesurface', 1)
+    elif args.resetsurface:
+        print("Telling surfaces to start over")
+        osc_dispatch('/resetsurface', 1)
+    elif args.surface:
+        print("Sending Surface Message")
+        ## foo = json.loads('{"number": 1.0, "other": 4.3}')
+        osc_dispatch('/surface-sentiments', '{"sentiment": 0.15, "focus": 0.65, "energy": -0.3, "unit": "test", "words": ["inspired", "anxious", "understanding"], "parts": ["hand", "eye", "head"]}')
+
---
+++ util.py
@@ -0,0 +1,43 @@
+from pythonosc import dispatcher, osc_server, udp_client, osc_message_builder
+import logging 
+# const
+ip_osc = '192.168.1.255'
+##ip_osc = '192.168.0.255'
+ip_osc_server='0.0.0.0'
+ip_osc_editor='196.168.1.255'
+## ip_osc = '10.253.0.255'
+port_server = 7007
+port_client = 7007
+port_client_editor = 7007
+api_url = "http://frankenstein.hunterowens.net/"
+
+logger = logging.getLogger('util.module')
+logger.setLevel(logging.INFO)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('osc_server.log')
+fh.setLevel(logging.INFO)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+def osc_dispatch(addr, msg, ip=ip_osc_server, port=port_client, num_tries=1):
+    """
+    Dispatches a message in state change over OSC to all listeners
+    """
+    client = udp_client.UDPClient(ip, port,1)
+    ## SimpleOSCClientRedux(client)
+    ## client._sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    
+    builder = osc_message_builder.OscMessageBuilder(address=addr)
+    builder.add_arg(msg)
+    for _ in range(num_tries):
+        client.send(builder.build())
+        logger.info("Sending {0} with {1} to {2} at {3}, Attempt {4}".format(addr, msg, ip, port, _))
+    return None
