a99722336c36b3b1529d30123dfddda6695648a5
hunterowens <owens.hunter@gmail.com>
2018-06-04 17:19:57 -0700

    questions working

--- dialogflow/index.js
+++ dialogflow/index.js
@@ -1,7 +1,29 @@
+
+
 exports.helloHttp = function helloHttp (request, response) {
-  var fs = require('fs');
-  var questions = fs.readFileSync('questions.csv').toString().split("\n");
-  
-  var question = questions[Math.floor(Math.random()*questions.length)]
-  response.json({ fulfillmentText: question});
+  var Airtable = require('airtable');
+  var base = new Airtable({apiKey: 'keyLoE3zbLtTbI3VU'}).base('appTUc1Aznap8TIfJ');
+  var questions = [];
+  base('Table 1').select({
+      // Selecting the first 3 records in Grid view:
+      maxRecords: 3,
+      view: "Grid view"
+  }).eachPage(function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+
+      records.forEach(function(record) {
+          console.log('Retrieved', record.get('Question Text'));
+          questions.push(record.get('Question Text'));
+      });
+
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+
+  }, function done(err) {
+      if (err) { console.error(err); return; }
+      response.json({ fulfillmentText: questions[Math.floor(Math.random()*questions.length)]});
+  });
+    
 };
